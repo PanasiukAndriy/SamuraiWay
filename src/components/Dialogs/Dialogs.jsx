b2e192916df6f35d95101300a8b207298018f703
NERD-PC\Nerd <panasiukandriy87@gmail.com>
2022-10-22 21:22:01 +0300

    dialogs layout

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,36 @@ import s from './Dialogs.module.css'
 const Dialogs = () => {
     return (
         <div>
-            dialogs
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog}>
+                        dima
+                    </div>
+                    <div className={s.dialog}>
+                        valera
+                    </div>
+                    <div className={s.dialog}>
+                        ira
+                    </div>
+
+                </div>
+
+                <div className={s.messages}>
+                    <div className={s.message}>
+                        hi
+                    </div>
+                    <div className={s.message}>
+                        how are you
+                    </div>
+                    <div className={s.message}>
+                        okk
+                    </div>
+
+                </div>
+
+
+            </div>
+
         </div>
     )
 }
