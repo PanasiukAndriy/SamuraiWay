ff234ad51bf187837c9085de4013e8f548dbc62e
NERD-PC\Nerd <panasiukandriy87@gmail.com>
2023-03-17 20:18:11 +0200

    finish 47 copy state in profileReducer

--- src/index.js
+++ src/index.js
@@ -7,23 +7,16 @@ import App from "./App";
 import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let rerenderEntireTree = (state) => {
-    root.render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </React.StrictMode>
-    );
-}
 
-rerenderEntireTree(store.getState());
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>
+);
 
-store.subscribe(() => {
-    let state = store.getState();
-    rerenderEntireTree(state);
 
-});
 
 
 
