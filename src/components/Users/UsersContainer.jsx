8649ae2f1816de4cde0a7b0987ed2268666c773d
DESKTOP-UVFVG0C\Nerd <panasiukandriy87@gmail.com>
2023-04-25 22:48:41 +0300

    finish 55 pagination

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import {connect} from "react-redux";
-import {followActionCreator, setUsersActionCreator, unfollowActionCreator} from "../../redux/usersReducer";
+import {
+    followActionCreator,
+    setCurrentPageActionCreator, setTotalUsersCountActionCreator,
+    setUsersActionCreator,
+    unfollowActionCreator
+} from "../../redux/usersReducer";
 import Users from "./Users";
 
 
 let mapStateToProps = (state) => {
     return{
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 
@@ -14,7 +22,10 @@ let mapDispatchToProps = (dispatch) => {
     return {
         follow: (userID) => {dispatch(followActionCreator(userID))},
         unfollow: (userID) => {dispatch(unfollowActionCreator(userID))},
-        setUsers: (users) => {dispatch(setUsersActionCreator(users))}
+        setUsers: (users) => {dispatch(setUsersActionCreator(users))},
+        setCurrentPage: (pageNumber) => {dispatch(setCurrentPageActionCreator(pageNumber))},
+        setTotalUsersCount: (totalCount) => {dispatch(setTotalUsersCountActionCreator(totalCount))},
+
     }
 
 }
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -1,18 +1,17 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
+
 const urlImg = 'https://variety.com/wp-content/uploads/2021/04/Avatar.jpg?w=681&h=383&crop=1&resize=681%2C383';
 
 
 let initialState = {
-    users: [
-        /*{id: 1, photoURL: urlImg, followed: true, fullName: 'Dima', status: 'my statysss', location: {city: 'kiev', country:'UA'}},
-        {id: 2, photoURL: urlImg, followed: false, fullName: 'Andrew', status: 'my statysss', location: {city: 'london', country:'GB'}},
-        {id: 3, photoURL: urlImg, followed: true,  fullName: 'Sasha', status: 'my statysss', location: {city: 'LA', country:'USA'}},
-        {id: 4, photoURL: urlImg, followed: false,  fullName: 'Oleh', status: 'my statysss', location: {city: 'Dakota', country:'NA'}},
-        {id: 5, photoURL: urlImg, followed: true,  fullName: 'Peter', status: 'my statysss', location: {city: 'Sydney', country:'Australia'}},
-        {id: 6, photoURL: urlImg, followed: false,  fullName: 'Olesja', status: 'my statysss', location: {city: 'Lissabon', country:'NA'}}*/
-    ]
+    users: [],
+    pageSize: 15,
+    totalUsersCount: 0,
+    currentPage: 2
 }
 
 const usersReducer = (state = initialState, action) =>
@@ -39,7 +38,11 @@ const usersReducer = (state = initialState, action) =>
                 }) }
             return  stateCopy;
         case SET_USERS:
-             return  {...state, users: [...state.users, ...action.users]}
+             return  {...state, users: action.users}
+        case SET_CURRENT_PAGE:
+             return  {...state, currentPage: action.currentPage}
+        case SET_TOTAL_USERS_COUNT:
+            return  {...state, totalUsersCount: action.count}
         default:
             return state;
     }
@@ -49,5 +52,8 @@ const usersReducer = (state = initialState, action) =>
 export const followActionCreator = (userID) => ({type: FOLLOW, userID });
 export const unfollowActionCreator = (userID) => ({type: UNFOLLOW, userID});
 export const setUsersActionCreator = (users) => ({type: SET_USERS, users});
+export const setCurrentPageActionCreator = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
+export const setTotalUsersCountActionCreator = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount});
+
 
 export default usersReducer;
